e72d1e6a244a6ef85b3232d5cc51942aeb7439cc
Michael Beamer <michael@Air-von-Michael.home>
2026-01-30 19:01:48 +0100

    feat: implement reverseString function
    Add a function to reverse a given string using a for-loop.

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  let reversedString = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    console.log(str[i]);
+    reversedString += str[i];
+  }
+  return reversedString;
 };
 
+console.log(reverseString("hello there"));
 // Do not edit below this line
 module.exports = reverseString;
