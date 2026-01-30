7fdf7c9f59052472d8a5cb6a8034a958ed42f076
Michael Beamer <michael@Air-von-Michael.home>
2026-01-30 20:53:53 +0100

    feat: implement removeFromArray function
    Add a function to remove a given element or number of elements from an array using filter.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...elementsToRemove) {
+  return arr.filter((element) => !elementsToRemove.includes(element));
 };
+
+console.log(removeFromArray([1, 2, 2, 3, 4], 2, 3, 4));
 
 // Do not edit below this line
 module.exports = removeFromArray;
