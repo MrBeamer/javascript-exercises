8ca757eec844bed3867b62df6dc1550427cc8319
Michael Beamer <michael@Air-von-Michael.home>
2026-01-30 17:27:48 +0100

    feat: implement repeatString function
    Add a function to repeat a given string 'n' times using a for-loop.
    
    Include a check to return 'ERROR' if the number provided is negative.
    
    Avoid using the built-in String.prototype.repeat as per task requirements.

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
+const repeatString = function (str, numb) {
+  let repeatedString = "";
+  if (numb < 0) return "ERROR";
+  for (let i = 0; i < numb; i++) {
+    repeatedString += str;
+  }
 
+  return repeatedString;
 };
 
+repeatString("hey", 3);
 // Do not edit below this line
 module.exports = repeatString;
