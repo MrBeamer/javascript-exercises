e279f952148c65f3b86d3550880ad827fd63db10
Michael Beamer <michael@Air-von-Michael.home>
2026-01-31 20:18:05 +0100

    feat: implement sumAll function
    Add a function to sum all given numbers in range and safeguards against none integers.

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function (numb1, numb2) {
+  let numberSum = 0;
 
+  if (
+    Number.isInteger(numb1) &&
+    numb1 > 0 &&
+    Number.isInteger(numb2) &&
+    numb2 > 0
+  ) {
+    if (numb1 > numb2) {
+      let tempNumb = numb1;
+      numb1 = numb2;
+      numb2 = tempNumb;
+    }
+
+    for (let i = numb1; i <= numb2; i++) {
+      numberSum += i;
+    }
+    return numberSum;
+  } else return "ERROR";
 };
-
+console.log(sumAll(-10, 4));
 // Do not edit below this line
 module.exports = sumAll;
