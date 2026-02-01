b449b0ca55a0281206d0cf0339c6b7c5eb8e2445
Michael Beamer <michael@Air-von-Michael.home>
2026-02-01 14:32:30 +0100

    feat: implement tempConversion function
    Add function that converts Fahrenheit to Celsius and vice versa.

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,31 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temperature) {
+  const temperatureCelsius = (temperature - 32) * (5 / 9);
+  const roundedTemperatureCelsius = Math.round(temperatureCelsius * 10) / 10;
+  return roundedTemperatureCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temperature) {
+  const temperatureFahrenheit = temperature * 1.8 + 32;
+  const roundedTemperatureFahrenheit =
+    Math.round(temperatureFahrenheit * 10) / 10;
+  return roundedTemperatureFahrenheit;
 };
+console.log(convertToCelsius(32));
+// console.log(convertToFahrenheit(0));
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
+/*
+Write two functions that convert temperatures from Fahrenheit to Celsius, and vice versa:
+
+convertToCelsius(32) // fahrenheit to celsius, should return 0
+
+convertToFahrenheit(0) // celsius to fahrenheit, should return 32
+Because we are human, we want the result temperature to be rounded to one decimal place: i.e., convertToCelsius(100) should return 37.8 and not 37.77777777777778.
+
+°F = (celsius number × 1.8) + 32
+°C = (fahrenheit number -32) /32
+*/
