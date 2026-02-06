1b9f41f95ea59bf96671e1642c7418603e1b0059
Michael Beamer <michael@Air-von-Michael.home>
2026-02-06 20:05:04 +0100

    feat: implement calculator functions
    Add functions fractional, power, add, subtract, division.

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,26 +1,37 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (arrNumb) {
+  return arrNumb.reduce((acc, current) => {
+    return acc + current;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arrNumb) {
+  return arrNumb.reduce((acc, current) => {
+    return acc * current;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function (base, exponent) {
+  return Math.pow(base, exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let factorialSum = 1;
+  for (let i = num; i > 0; i--) {
+    console.log(i);
+    factorialSum *= i;
+  }
+  return factorialSum;
 };
+
+console.log(factorial(0));
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +40,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
