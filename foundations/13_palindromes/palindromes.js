b1e989a784a46cecc517b0f4bef0b16dfe37865b
Michael Beamer <michael@Air-von-Michael.home>
2026-02-06 21:39:16 +0100

    feat: implement palindrome test function
    Add function that test if str is an palindrome or not (using charCodeAt and reverse).

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let lowercaseStr = str.toLowerCase();
+  let sanitizedStr = "";
+  for (let i = 0; i < lowercaseStr.length; i++) {
+    if (
+      (lowercaseStr.charCodeAt(i) > 47 && lowercaseStr.charCodeAt(i) < 58) ||
+      (lowercaseStr.charCodeAt(i) > 96 && lowercaseStr.charCodeAt(i) < 123)
+    ) {
+      sanitizedStr += lowercaseStr[i];
+    }
+  }
+  let reversedStr = sanitizedStr.split("").reverse().join("");
+  console.log(sanitizedStr);
+  console.log(reversedStr);
+  return sanitizedStr === reversedStr ? true : false;
 };
+
+console.log(palindromes("racecar"));
+console.log(palindromes("r3ace3car"));
 
 // Do not edit below this line
 module.exports = palindromes;
