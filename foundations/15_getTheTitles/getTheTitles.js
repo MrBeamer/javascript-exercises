ac8ed5440d87703ed96c35b40d27a3aed07b98b9
Michael Beamer <michael@Air-von-Michael.home>
2026-02-07 12:43:51 +0100

    feat: Add getTitle function
    Add function that gets book titles from an array with objects - using map.

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,6 +1,26 @@
-const getTheTitles = function() {
+const books = [
+  {
+    title: "Book",
+    author: "Name",
+  },
+  {
+    title: "Book2",
+    author: "Name2",
+  },
+];
 
+const getTheTitles = function () {
+  const bookTitles = books.map((book) => book.title);
+  return bookTitles;
 };
 
+getTheTitles(books); // ['Book','Book2']
 // Do not edit below this line
 module.exports = getTheTitles;
+
+/*
+Your job is to write a function that takes the array and returns an array of titles:
+
+getTheTitles(books) // ['Book','Book2']
+Hints
+You should use a built-in javascript method to do most of the work for you!*/
