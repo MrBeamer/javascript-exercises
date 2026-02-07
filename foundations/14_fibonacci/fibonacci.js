a3ed1e245510c9d395cee8bdd9bc22aaa2d52f50
Michael Beamer <michael@Air-von-Michael.home>
2026-02-07 12:36:00 +0100

    feat: Add function Fibonacci sequence
    Add a function to get the nth position of Fibonacci sequence - ladder approach.

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function (positionNumber) {
+  if (isNaN(positionNumber) || positionNumber < 0) return "OOPS";
+  let fibonacciSequence = [];
+  for (let i = 0; i <= positionNumber; i++) {
+    if (i === 0 || i === 1) {
+      fibonacciSequence.push(i);
+    } else {
+      fibonacciSequence.push(
+        fibonacciSequence[i - 1] + fibonacciSequence[i - 2],
+      );
+    }
+  }
+  return fibonacciSequence[positionNumber];
 };
+fibonacci(0);
 
 // Do not edit below this line
 module.exports = fibonacci;
