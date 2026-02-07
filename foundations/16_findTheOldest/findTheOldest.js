2dae821605886a6e8496ebbb02d5d2a88178d124
Michael Beamer <michael@Air-von-Michael.home>
2026-02-07 14:59:45 +0100

    feat: Add the findTheOldest function
    Add function that finds the oldest person in an array

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  const updatedPeople = people
+    .map((person) => {
+      const currentYear = new Date().getFullYear();
+      let yearOfDeath = person.yearOfDeath;
+      if (yearOfDeath === undefined) yearOfDeath = currentYear;
+      return {
+        name: person.name,
+        age: yearOfDeath - person.yearOfBirth,
+        yearOfBirth: person.yearOfBirth,
+        yearOfDeath: yearOfDeath,
+      };
+    })
+    .sort((a, b) => b.age - a.age);
+  return updatedPeople[0];
 };
+findTheOldest(people);
 
-// Do not edit below this line
 module.exports = findTheOldest;
